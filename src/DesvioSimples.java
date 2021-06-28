9a98f48c546d01f4c987a7b86cee56dbb9bfca54
Joao Bizzarri <joaobizzarri@gmail.com>
2021-06-27 22:23:19 -0300

    Implementandos os desvios condicionais

--- src/DesvioSimples.java
+++ src/DesvioSimples.java
@@ -14,6 +14,13 @@ public class DesvioSimples {
 		System.out.println("Deixe-me verificar sua idade...");
 		/*Aqui devemos criar uma solução para verificar a idade do usuário*/
 		
+		//abaixo temos um if simples que é responsavel por realizar uma tarefa apenas quando o resultado do teste é verdadeiro //
+		if (idade>=12) {
+			System.out.println("Você pode jogar");
+			
+		};
+		
+		
 		leitor.close();
 
 	}
